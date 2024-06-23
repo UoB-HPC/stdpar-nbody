7347243ecc3893e1a1600d4c326401a675d526a2
Gonzalo Brito Gadeschi <gonzalob@nvidia.com>
2024-06-23 11:24:51 -0700

    Workaround nvc++ stdpar=multicore bug

--- src/counting_iterator.h
+++ src/counting_iterator.h
@@ -3,15 +3,17 @@
 
 #include <ranges>
 
-// Workaround for nvc++ not supporting 128-bit integers yet
-// used by std::views::iota(64-bit int) overflow checking:
-#if defined(_NVHPC_STDPAR_GPU)
+// Workaround for two nvc++ bugs:
+// - Not supporting 128-bit integers yet on GPUs,
+//   used by std::views::iota(64-bit int) overflow checking.
+// - Link failure with OpenMP on CPU due to unknown issue yet.
+#if defined(_NVHPC_STDPAR_GPU) || defined(_NVHPC_STDPAR_MULTICORE)
 #include <thrust/iterator/counting_iterator.h>
 #endif
 
 template <typename T>
-auto counting_iterator(T v = T(0)) {
-  #if defined(_NVHPC_STDPAR_GPU)
+constexpr auto counting_iterator(T v = T(0)) {
+  #if defined(_NVHPC_STDPAR_GPU) || defined(_NVHPC_STDPAR_MULTICORE)
       return thrust::counting_iterator<T>(T(v));
   #else
       return std::views::iota(T(v)).begin();
