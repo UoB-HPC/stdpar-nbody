bce2ae08dab3566762b0578b619dd06b444fef9c
Gonzalo Brito Gadeschi <gonzalob@nvidia.com>
2024-08-05 05:24:08 -0700

    Add macro for unsafely using par_unseq instead of par

--- src/execution.h
+++ src/execution.h
@@ -4,6 +4,9 @@
 #if defined(SEQUENTIAL)
 inline constexpr auto par       = std::execution::seq;
 inline constexpr auto par_unseq = std::execution::seq;
+#elif defined(UNSAFE_PAR_UNSEQ)
+inline constexpr auto par       = std::execution::par_unseq;
+inline constexpr auto par_unseq = std::execution::par_unseq;
 #else
 inline constexpr auto par       = std::execution::par;
 inline constexpr auto par_unseq = std::execution::par_unseq;
